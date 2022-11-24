d135e060b7cbf24735958168d1592d0ea8061940
Macqyntajz <Macqyntajz.gonzales@gmail.com>
2022-11-24 09:09:26 +0800

    Create otp.java

---
+++ otp.java
@@ -0,0 +1,26 @@
+package prog_1;
+import java.util.Scanner;
+import java.util.Random;
+public class otp {
+
+	public static void main(String[] args) {
+		
+		int random = 0;
+		String [] ranum = new String [6];
+		
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter OTP");
+		System.out.print(random);
+		
+		int ranum2 = input.nextInt();
+		
+	
+		while(random < 6) {
+			
+			int numbers = ranum.length;	
+			
+			
+			
+		}
+	}
+}
